9443ca7719a8df3f390f1c94bc7e046e13d7f501
hackpro4005 <114504302+hackpro4005@users.noreply.github.com>
2023-09-21 12:42:54 +0300

    Add files via upload

---
+++ build.js
@@ -0,0 +1,13 @@
+{
+  "name"; "Theme set 1";
+  "version"; "0.0.1";
+  "description"; "A theme set";
+  "source"; [
+    "theme/gbpocket.json",
+    "theme/green.json",
+    "theme/lightpurple.json",
+    "theme/blue.json",
+  ];
+  "output"; "./dist/themeset1.vsix";
+  "license"; "MIT";
+}
